c423308d84ec127708c216fb38eaf47809974790
33TU <eetu.programmer@gmail.com>
2026-08-01 17:55:46 +0300

    Add cross-platform Docker launchers

---
+++ bin/crossbridge.ps1
@@ -0,0 +1,37 @@
+[CmdletBinding()]
+param(
+    [Parameter(Position = 0, ValueFromRemainingArguments = $true)]
+    [string[]] $Command
+)
+
+$ErrorActionPreference = "Stop"
+
+$image = if ([string]::IsNullOrWhiteSpace($env:CROSSBRIDGE_IMAGE)) {
+    "crossbridge:15.0.0.3-light"
+} else {
+    $env:CROSSBRIDGE_IMAGE
+}
+
+$dockerArguments = @(
+    "run"
+    "--rm"
+    "--mount"
+    "type=bind,source=$($PWD.Path),target=/work"
+    "-e"
+    "FLEX=/opt/flex"
+)
+
+if (-not [Console]::IsInputRedirected -and -not [Console]::IsOutputRedirected) {
+    $dockerArguments += "-it"
+}
+
+$dockerArguments += $image
+
+if ($null -eq $Command -or $Command.Count -eq 0) {
+    $dockerArguments += "bash"
+} else {
+    $dockerArguments += $Command
+}
+
+& docker @dockerArguments
+exit $LASTEXITCODE
